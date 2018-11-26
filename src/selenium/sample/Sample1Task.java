389a9fbd2c2acf231bbcf47a4fa0bda1ceedbd3a
MahdiFaycel <bentahar.mahdifaycel@gmail.com>
2018-11-26 11:46:41 +0200

    Changes

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -2,6 +2,9 @@ package selenium.sample;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
@@ -9,6 +12,28 @@ public class Sample1Task {
     @Test
     public void goToHomepage() throws Exception {
 //         TODO:
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/site/index2.html");
+      //  System.out.println(driver.findElement(By.id("h1")).getText());
+        //get title of page
+        System.out.println(driver.getTitle());
+
+        //get URL of current page
+        System.out.println(driver.getCurrentUrl());
+
+        //Sleep for 10 seconds
+        Thread.sleep(5000);
+
+        //Close browser
+        driver.close();
+
+
+
+
+
+
 //         define driver
 //         go to https://kristinek.github.io/site/index2.html
 //         get title of page
--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -19,7 +23,7 @@ public class Sample2Task {
         driver = new ChromeDriver();
 
         //open page:
-        driver.get("https://kristinek.github.io/site/examples/loc");
+        driver.get("https://kristinek.github.io/site/examples/locators");
     }
 
     // method which is being run after each test
@@ -33,6 +37,8 @@ public class Sample2Task {
 //         TODO:
 //         get text "Heading 2 text" using id
 //        <h2 id="heading_2">Heading 2 text</h2>
+
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
@@ -40,12 +46,19 @@ public class Sample2Task {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
 //        <input type="button" id="buttonId" value="This is also a button" name="randomButton2">
+
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value")+" \t " +
+                driver.findElement(By.name("randomButton2")).getAttribute("id"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+
+        System.out.println(driver.findElement(By.className("test")).getText());
+
+
     }
 
     @Test
@@ -54,5 +67,16 @@ public class Sample2Task {
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
 //         get third text of class "test" (should be "Test Text 4")
+
+        System.out.println(driver.findElements(By.className("test")).size());
+
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+
+        for (WebElement elementWithClass : allElementsWithClass) {
+            System.out.println(elementWithClass.getText());}
+        System.out.println();
+      //  System.out.println(driver.findElements(By.className("text")).get(0).getText());
+        System.out.println(driver.findElements(By.className("test")).get(2).getText());
+
     }
 }
