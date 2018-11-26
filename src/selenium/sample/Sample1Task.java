cd00ec2f9a9907c60add4f44abdac0c1874a6306
JurisB-Student <jurbiz@inbox.lv>
2018-11-26 11:46:24 +0200

    2 tasks with changes

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -2,6 +2,9 @@ package selenium.sample;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample1Task {
     static String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
@@ -9,10 +12,17 @@ public class Sample1Task {
     @Test
     public void goToHomepage() throws Exception {
 //         TODO:
+
 //         define driver
+           System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+           WebDriver driver = new ChromeDriver();
 //         go to https://kristinek.github.io/site/index2.html
-//         get title of page
-//         get URL of current page
-//         close browser
+
+           driver.get("https://kristinek.github.io/site/index2.html");
+           System.out.println(driver.getTitle());
+           System.out.println(driver.findElement(By.id("h1")).getText());
+           System.out.println(driver.getCurrentUrl());
+
+           driver.close();
     }
 }
--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -33,26 +37,42 @@ public class Sample2Task {
 //         TODO:
 //         get text "Heading 2 text" using id
 //        <h2 id="heading_2">Heading 2 text</h2>
+        System.out.println(driver.findElement(By.id ("heading_2")).getText());
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
+
 //        <input type="button" id="buttonId" value="This is also a button" name="randomButton2">
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value") + " \t  " +
+                driver.findElement(By.name("randombutton2")).getAttribute("id")) ;
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElement(By.className("test")).getText());
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
 //         TODO:
 //         get size text of class "test" (should be 5)
+        //System.out.println(driver.get (By.className("test")).getText());getClass();
 //         get text of class "test"
+
+        System.out.println(driver.findElements(By.className("test")).size());
+
+        List<WebElement> allElementWithClass = driver.findElements(By.className("test")).
+
+             for(WebElement elementWithClass : allElementsWithClass)   {
+                 System.out.println(elementWithClass.getText());}
+        System.out.println();
 //         get third text of class "test" (should be "Test Text 4")
+
+        System.out.println(driver.findElements(By.className("test")).get(2).getText());
     }
 }
