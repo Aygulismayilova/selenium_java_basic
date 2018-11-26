bb8fb1101d75fd5a9ab87dc3c039268560d2743e
Nithingithub <nithin007ram@gmail.com>
2018-11-26 11:46:20 +0200

    some changes

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,8 +3,10 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.yaml.snakeyaml.events.Event;
 
 public class Sample2Task {
     WebDriver driver;
@@ -19,7 +21,7 @@ public class Sample2Task {
         driver = new ChromeDriver();
 
         //open page:
-        driver.get("https://kristinek.github.io/site/examples/loc");
+        driver.get("https://kristinek.github.io/site/examples/locators");
     }
 
     // method which is being run after each test
@@ -31,13 +33,18 @@ public class Sample2Task {
     @Test
     public void findElementByID() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
+
 //         get text "Heading 2 text" using id
 //        <h2 id="heading_2">Heading 2 text</h2>
+
     }
 
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.name("Button2")));get
+
 //         get attribute "id" and "value" of button "This is also a button" using name
 //        <input type="button" id="buttonId" value="This is also a button" name="randomButton2">
     }
@@ -45,12 +52,14 @@ public class Sample2Task {
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.className("test")));getClass();
 //         get first text of class "test" (should be "Test Text 1")
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
 //         TODO:
+        System.out.println(driver.findElements(By.className("test")));getClass();
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
 //         get third text of class "test" (should be "Test Text 4")
