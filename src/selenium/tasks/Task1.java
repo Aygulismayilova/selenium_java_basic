0f23a15849c614bbd7c0722b0cbdc64e1eed74c4
KarlisLemans <leimanislemans@gmail.com>
2018-11-26 17:26:22 +0200

    Changes in task1

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -1,10 +1,18 @@
 package selenium.tasks;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Task1 {
     WebDriver driver;
@@ -15,7 +23,7 @@ public class Task1 {
         String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://kristinek.github.io/test-sample/tasks/task1");
+        driver.get("https://kristinek.github.io/site/tasks/task1");
     }
 
     @After
@@ -27,12 +35,26 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+            WebElement text = driver.findElement(By.id("numb"));
+            String newText = "Word";
+            text.sendKeys(newText);
+            WebElement button = driver.findElement(By.className(" w3-orange"));
+            button.click();
+            assertEquals("Please enter a number", driver.findElement(By.id("ch1_error")).getText());
+
     }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        //When type 49, no error is seen
+        WebElement text = driver.findElement(By.id("numb"));
+        String newText = "48";
+        text.sendKeys(newText);
+        WebElement button = driver.findElement(By.className(" w3-orange"));
+        button.click();
+        assertEquals("Number is too small", driver.findElement(By.id("ch1_error")).getText());
     }
 
     @Test
@@ -41,6 +63,13 @@ public class Task1 {
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement text = driver.findElement(By.id("numb"));
+        String newText = "101";
+        text.sendKeys(newText);
+        WebElement button = driver.findElement(By.className(" w3-orange"));
+        button.click();
+        assertEquals("Number is too big", driver.findElement(By.id("ch1_error")).getText());
+
     }
 
     @Test
@@ -48,6 +77,21 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+        WebElement text = driver.findElement(By.id("numb"));
+        String newText = "64";
+        double foo = Integer.parseInt(newText);
+        foo = Math.sqrt(foo);
+        text.sendKeys(newText);
+        WebElement button = driver.findElement(By.className(" w3-orange"));
+        String elementTextOnPage = driver.findElement(By.id("ch1_error")).getText();
+        button.click();
+        Alert alert = driver.switchTo().alert();
+        assertTrue("Square root of" + newText + "is" + foo, true);
+        assertEquals("Square root of 64 is 8.00", alert.getText());
+        alert.accept();
+        assertFalse(elementTextOnPage,false);
+
+
     }
 
     @Test
@@ -55,5 +99,19 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        WebElement text = driver.findElement(By.id("numb"));
+        String newText = "54";
+        String elementTextOnPage = driver.findElement(By.id("ch1_error")).getText();
+        double foo = Integer.parseInt(newText);
+        foo = Math.sqrt(foo);
+        text.sendKeys(newText);
+        WebElement button = driver.findElement(By.className(" w3-orange"));
+
+        button.click();
+        Alert alert = driver.switchTo().alert();
+        assertTrue("Square root of" + newText + "is" + foo, true);
+        assertEquals("Square root of 54 is 7.35", alert.getText());
+        alert.accept();
+        assertFalse(elementTextOnPage,false);
     }
 }
