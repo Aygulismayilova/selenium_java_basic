2e1a4c340a5273f2b0b0d0a6088f7ddbac7be658
jekkart <jekaterina.kartasova@gmail.com>
2018-11-26 11:46:29 +0200

    new

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -19,7 +23,7 @@ public class Sample2Task {
         driver = new ChromeDriver();
 
         //open page:
-        driver.get("https://kristinek.github.io/site/examples/loc");
+        driver.get("https://kristinek.github.io/site/examples/locators");
     }
 
     // method which is being run after each test
@@ -31,6 +35,7 @@ public class Sample2Task {
     @Test
     public void findElementByID() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
 //         get text "Heading 2 text" using id
 //        <h2 id="heading_2">Heading 2 text</h2>
     }
@@ -38,6 +43,9 @@ public class Sample2Task {
     @Test
     public void findElementByName() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id"));
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("value"));
+
 //         get attribute "id" and "value" of button "This is also a button" using name
 //        <input type="button" id="buttonId" value="This is also a button" name="randomButton2">
     }
@@ -45,6 +53,7 @@ public class Sample2Task {
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
+        System.out.println(driver.findElement(By.className("test")).getText());
 //         get first text of class "test" (should be "Test Text 1")
     }
 
@@ -52,7 +61,20 @@ public class Sample2Task {
     public void findElementByClassAll() throws Exception {
 //         TODO:
 //         get size text of class "test" (should be 5)
-//         get text of class "test"
-//         get third text of class "test" (should be "Test Text 4")
+        System.out.println(driver.findElements(By.className("test")).size());
+        //         get text of class "test"
+        List<WebElement> allElementsWithClass = driver.findElements(By.className("test"));
+      //for (int i = 0, i < allElementsWithClass.size());
+        
+         for (WebElement elementWithClass : allElementsWithClass) {
+            System.out.println(elementWithClass.getText());
+
+        }
+        //         get third text of class "test" (should be "Test Text 4")
+        System.out.println("-----------------------");
+        System.out.println(driver.findElements(By.className("test")).get(2).getText());
+
+
+
     }
 }
