ba17100c777b5e921d6d8f12c263daa0c2017fbb
Nithingithub <nithin007ram@gmail.com>
2018-11-27 12:53:04 +0200

    some changes

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,6 +3,7 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,24 +33,29 @@ public class Task1 {
     @Test
     public void errorOnText() {
 //        TODO
+        WebElement textFieldGrey = driver.findElement(By.className("w3-light-grey"));
+        textFieldGrey.sendKeys("text");
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        submitButton.click();
+        WebElement submitError = driver.findElement(By.id("ch1_error"));
+        assert true(submitError, "Please enter a number");
+
 
 
 //        enter a text instead of a number, check that correct error is seen
-        driver.findElement(By.className(" w3-row")).click();
-        WebElement Enternumber = driver.findElement(By.id("numb"));
-        String expected = "hello";
-        Enternumber .clear();
-        Enternumber .sendKeys(expected);
-        driver.findElement(By.className(" w3-orange")).click();
-        assert equals(("Please enter a number",driver.findElement(By.id("ch1_error")).getText());
 
-            }
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
-//        enter number whichdr is too small (below 50), check that correct error is seen
-        driver.findElements(By.className(""));
+        WebElement FieldGrey = driver.findElement(By.className("w3-light-grey"));
+        textFieldGrey.sendKeys("34");
+        WebElement submit = driver.findElement(By.className("w3-orange"));
+        submitButton.click();
+        WebElement Error = driver.findElement(By.id("ch1_error"));
+        assert true(submitError.getText(), "Number is too small");
+    }
+
     }
 
     @Test
@@ -57,12 +63,31 @@ public class Task1 {
 
 //        BUG: if I enter number 666 no errors where seen
 //        TODO
+        WebElement textFieldGrey = driver.findElement(By.className("w3-light-grey"));
+        textFieldGrey.sendKeys("220");
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        submitButton.click();
+        WebElement submitError = driver.findElement(By.id("ch1_error"));
+        assert true(submitError.getText(), "Number is too big");
+
+
+
 //        enter number which is too big (above 100), check that correct error is seen
     }
 
     @Test
     public void correctSquareRootWithoutRemainder() {
 //        TODO
+
+        WebElement textFieldGrey = driver.findElement(By.className("w3-light-grey"));
+        textFieldGrey.sendKeys("64");
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        submitButton.click();
+        Alert alert = driver.switchTo().alert();
+        assert true("Square root of 81 is 8.00", alert.getText();
+    }
+
+
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
     }
@@ -70,6 +95,12 @@ public class Task1 {
     @Test
     public void correctSquareRootWithRemainder() {
 //        TODO
+        WebElement textFieldGrey = driver.findElement(By.className("w3-light-grey"));
+        textFieldGrey.sendKeys("65");
+        WebElement submitButton = driver.findElement(By.className("w3-orange"));
+        submitButton.click();
+        Alert alert = driver.switchTo().alert();
+        assert true("Square root of 65 is 8.06", alert.getText();
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
     }
